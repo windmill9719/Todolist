4067d1d7847aa9fbb04706e9a392f2ae171c7583
ErinLiu <1125489858@qq.com>
2019-11-01 02:55:29 +0800

    add style

--- src/components/project/ItemBar.vue
+++ src/components/project/ItemBar.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="item-bar">
-    <div class="item-text"><slot></slot></div>
-    <button @click="onDone">done</button>
-    <button>delete</button>
-  </div>
+
 </template>
 
 <script>
@@ -18,18 +14,5 @@
 </script>
 
 <style scoped>
-  .item-bar{
-    height: 50px;
-    border: 2px solid #eee;
-    margin-bottom: 2px;
-  }
-
-  .item-bar .item-text {
-    display: inline-block;
-    text-align: center;
-    width: 70%;
-    font-size: 30px;
-    list-style: none;
-  }
 
 </style>
--- src/components/project/MainArea.vue
+++ src/components/project/MainArea.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="main-area">
     <div class="main-wrapper">
-      <div class="content">
-        <div>
-          <input type="text" v-model="input" @submit.prevent>
-          <button @click="onAdd">add</button>
-          <button>more</button>
-        </div>
-        <ItemBar v-for="(todo, index) in todos"
-                 :key="index"
-                 @onItemDone="itemDone(index)" :class="{done: todo.isDone}">{{todo.text}}</ItemBar>
+      <div class="content item-bar">
+        <input type="text" v-model="input" @submit.prevent>
+        <button @click="onAdd">add</button>
+        <button>more</button>
       </div>
+      <div v-for="(todo, index) in todos"
+           :key="index"
+           :class="{done: todo.isDone}" class="item-bar">
+        <div class="item-text">{{todo.text}}</div>
+        <button @click="onDone(index)">done</button>
+        <button @click="onDelete(index)">delete</button>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
-  import ItemBar from "./ItemBar";
+  // import ItemBar from "./ItemBar";
   export default {
     name: "MainArea",
-    components:{
-      ItemBar
-    },
     data(){
       return{
         input: '',
@@ -49,9 +49,11 @@
         })
         this.input = ''
       },
-      itemDone(index){
+      onDone(index){
         this.todos[index].isDone = ! this.todos[index].isDone;
-
+      },
+      onDelete(index){
+        this.todos.splice(index,1)
       }
     }
   }
@@ -59,8 +61,36 @@
 
 <style scoped>
   .content input{
-    width: 80%;
-    height: 40px;
+    /*width: 80%;*/
+    height: 30px;
+    border: 0;
+    outline: none;
   }
 
+
+  .item-bar{
+    display: flex;
+    height: 50px;
+    border: 2px solid #eee;
+    margin-bottom: 2px;
+    align-items: center;
+  }
+
+  .item-bar input, .item-bar .item-text{
+    flex: 10;
+  }
+
+  .item-bar button {
+    flex: 1;
+    margin-right: 15px;
+  }
+
+
+  .item-bar .item-text {
+    display: inline-block;
+    text-align: center;
+    width: 70%;
+    font-size: 30px;
+    list-style: none;
+  }
 </style>
